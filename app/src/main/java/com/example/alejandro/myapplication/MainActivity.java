714e94f41b777edaa1482ec10b0faef8a63df710
lex919 <popovice.alexb@hotmail.com>
2015-10-22 15:55:08 +0200

    Add sout "hola mundo"

--- app/src/main/java/com/example/alejandro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alejandro/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //esto es un comentario
+        System.out.println("hola mundo");
     }
 }
